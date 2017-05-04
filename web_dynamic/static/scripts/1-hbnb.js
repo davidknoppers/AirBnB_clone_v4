0953827581946336b485461bd3662e1588b3186c
Richard Sim <rdsim8589@gmail.com>
2017-05-04 08:05:17 -0700

    jquery script to catch clicking action

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,27 @@
+let amenitiesIDs = new Map();
+$( document ).ready(function() {
+    $('input:checkbox').change(amenitiesCheckBox
+			      );
+});
+
+function amenitiesCheckBox() {
+    const amenID = $(this).attr("data-id");
+    const amenName = $(this).attr("data-name");
+    if ($(this).is(':checked')) {
+	amenitiesIDs.set(amenName, amenID);
+	}
+    else if ($(this).not(':checked')) {
+	amenitiesIDs.delete(amenName);
+    }
+    let names = "";
+    let listOfNames = amenitiesIDs.keys()
+    for (let name of listOfNames) {
+	if (names === "") {
+	    names = names.concat(name);
+	}
+	else {
+	    names = names.concat(", ", name);
+	}
+    }
+    $(".amenities h4").text(names);
+}
