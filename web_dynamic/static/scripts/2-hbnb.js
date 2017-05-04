2331860368dc1259efa39627fb2bd43119adf9f0
davidknoppers <david.knoppers@holbertonschool.com>
2017-05-04 23:56:40 +0000

    working, semistandard-compliant draft of 2

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,34 +1,35 @@
 let amenitiesIDs = new Map();
-$( document ).ready(function() {
-    $('input:checkbox').change(amenitiesCheckBox);
-    statusColor();
+$(document).ready(function () {
+  $('input:checkbox').change(amenitiesCheckBox);
+  statusColor();
 });
 
-function amenitiesCheckBox() {
-    const amenID = $(this).attr("data-id");
-    const amenName = $(this).attr("data-name");
-    if ($(this).is(':checked')) {
-	amenitiesIDs.set(amenName, amenID);
-	}
-    else if ($(this).not(':checked')) {
-	amenitiesIDs.delete(amenName);
+function amenitiesCheckBox () {
+  const amenID = $(this).attr('data-id');
+  const amenName = $(this).attr('data-name');
+  if ($(this).is(':checked')) {
+    amenitiesIDs.set(amenName, amenID);
+  } else if ($(this).not(':checked')) {
+    amenitiesIDs.delete(amenName);
+  }
+  let names = '';
+  let listOfNames = amenitiesIDs.keys();
+  for (let name of listOfNames) {
+    if (names === '') {
+      names = names.concat(name);
+    } else {
+      names = names.concat(', ', name);
     }
-    let names = "";
-    let listOfNames = amenitiesIDs.keys()
-    for (let name of listOfNames) {
-	if (names === "") {
-	    names = names.concat(name);
-	}
-	else {
-	    names = names.concat(", ", name);
-	}
-    }
-    $(".amenities h4").text(names);
+  }
+  $('.amenities h4').text(names);
 }
 
-
-function statusColor() {
-    $.get("http://0.0.0.0:5001/api/v1/status/", function (data, textStatus) {
-	alert("Response from server:" + data.status);
-    });
+function statusColor () {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
 }
